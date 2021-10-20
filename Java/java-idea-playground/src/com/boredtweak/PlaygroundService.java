6e3bed61e1db7a47f4e69264bcf1a38502f3b14e
BoredTweak <alex.elia37@gmail.com>
2021-10-20 13:23:08 -0700

    playing with java streams

--- Java/java-idea-playground/src/com/boredtweak/PlaygroundService.java
+++ Java/java-idea-playground/src/com/boredtweak/PlaygroundService.java
@@ -1,5 +1,10 @@
 package com.boredtweak;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
 public class PlaygroundService {
     public void Play(){
         var maxBananas = 25;
@@ -10,6 +15,7 @@ public class PlaygroundService {
         PlayWithNulls("banana");
         PlayWithConditionalAssignment(1);
         PlayWithConditionalAssignment(-1);
+        PlayWithStreams();
     }
 
     private void PlayWithNulls(String input) {
@@ -21,5 +27,22 @@ public class PlaygroundService {
     private void PlayWithConditionalAssignment(int input) {
         var result = 0 <= input ? "Greater Than Zero" : "Less than Zero";
         System.out.println(String.format("%d is %s", input, result));
+    }
+
+    private void PlayWithStreams() {
+        String[] names = {"Lewis Carrol", "H.G. Wells", "Michael Ende"};
+
+        // First First
+        System.out.println(Arrays.stream(names).findFirst());
+
+        // Filter
+        var namesWithL = Arrays.stream(names).filter((name) -> name.contains("L")).collect(toList());
+        for(String item: namesWithL){
+            System.out.println(item);
+        }
+
+        // Reduce
+        var charactersInNames = Arrays.stream(names).reduce(0, (subtotal, name) -> subtotal + name.length(), Integer::sum);
+        System.out.println(String.format("%d characters in names", charactersInNames));
     }
 }
