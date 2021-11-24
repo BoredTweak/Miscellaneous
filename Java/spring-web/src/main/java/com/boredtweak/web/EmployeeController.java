aea47d10ac10f8d5045e0c05a555364df2cba88b
BoredTweak <alex.elia37@gmail.com>
2021-11-24 14:59:11 -0800

    java experimentation

--- Java/spring-web/src/main/java/com/boredtweak/web/EmployeeController.java
+++ Java/spring-web/src/main/java/com/boredtweak/web/EmployeeController.java
@@ -2,21 +2,29 @@ package com.boredtweak.web;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+    private Set<Employee> _employees = ConcurrentHashMap.newKeySet();
+
     EmployeeController() {
     }
 
     @GetMapping("/employees")
-    List<Employee> all() {
-        return new ArrayList<Employee>() {
-            {
-                add(new Employee("bob", "staff"));
-            }
-        };
+    Set<Employee> all() {
+        return _employees;
+    }
+
+    @PostMapping("/employees")
+    Employee create(@RequestBody Employee input) {
+        _employees.add(input);
+        return input;
     }
 }
